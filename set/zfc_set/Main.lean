54eceffc0aacf66160ce2ed40832a4fd1d4c7dde
harodggg <haroldtiansheng@gmail.com>
2026-08-09 22:15:35 +0800

    feat: add zfc_set basic
    Signed-off-by: harodggg <haroldtiansheng@gmail.com>

--- set/zfc_set/Main.lean
+++
@@ -1,4 +0,0 @@
-import ZfcSet
-
-def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
--- set/zfc_set/ZfcSet/Basic.lean
+++ set/zfc_set/ZfcSet/Basic.lean
@@ -1,1 +1,15 @@
-def hello := "world"
+
+opaque Set : Type
+
+
+opaque mem : Set → Set → Prop
+
+infix:50 " ∈ " => mem
+
+def not_mem(x A: Set): Prop := ¬ (x ∈ A)
+infix:50 " ∉ " => not_mem
+
+
+def subset(A B: Set): Prop :=
+  ∀ x: Set,x ∈ A → x ∈ B
+infix:50 " ⊆ " => subset
